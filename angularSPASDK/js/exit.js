15b39c5ffd363545855cf8d66f1958e330240b52
Ronald Lobo <rlobo@trianglecrm.com>
2014-10-01 12:27:46 -0600

    added downsell exit logic

--- angularSPASDK/js/exit.js
+++ angularSPASDK/js/exit.js
@@ -1,12 +1,18 @@
-var internal;
-  if (internal != 1) {
-    setTimeout(function(){window.onunload = function(event){
-            if (internal != 1) {
-                window.onunload = null;
-                window.location.href = '#/'+ downsell;
-                event.preventDefault();
-            }
-    }},1000);
-  } else {
-    internal = 0;
-  }
+var internal = 0;
+window.onbeforeunload = function (e) {
+    if(internal != 1){
+        var message = "If you stay on the page you will be redirected to a new product",
+        e = e || window.event;
+        // For IE and Firefox
+        if (e) {
+        e.returnValue = message;
+        }
+        // For Safari
+        setTimeout(function(){
+            window.location.href = '#/'+ downsell+'?downsell=1';
+        },1000);
+        return message;
+    }
+    return ;
+};
+
